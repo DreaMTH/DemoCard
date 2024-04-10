5454980ffb0ab65483003f055e585639da9ea3e3
dreamth <d.dimcard@gmail.com>
2024-04-10 15:09:20 +0300

    add some variables

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { userModel } from "./models/userModel.js";
-//import { authValidator } from "./validators/auth.js";
+import { authValidator } from "./validators/auth.js";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import env from "dotenv";
@@ -11,11 +11,14 @@ mongoose
     `mongodb+srv://dreamth:${DB_PASS}@testclaster.jnzonwj.mongodb.net/?retryWrites=true&w=majority&appName=testClaster`,
   )
   .then(() => console.log(`${new Date()} Connected to the database...`))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(`${new Date()} ${err}`));
 const app = express();
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("meme2");
+app.get("/", async (req, res) => {
+  res.status(200).send("meme2");
+});
+app.post("/auth/reg", async (req, res) => {
+  res.status(200).send("meme3");
 });
 app.listen(PORT || 1337, (err) => {
   if (err) {
--- server/validators/userValidator.js
+++ server/validators/userValidator.js
@@ -3,5 +3,10 @@ import { body } from "express-validator";
 export const userValidator = [
   body("email").isEmail(),
   body("name").isLength({ min: 6 }),
-  body("password").isLength({ min: 8 }),
+  body("password").isStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minLowercase: 1,
+    minNumbers: 1,
+  }),
 ];
