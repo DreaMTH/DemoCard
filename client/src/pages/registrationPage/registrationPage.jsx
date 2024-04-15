b65ffdc32c08c0635af6543d72e4aec1367d15c4
dreamth <d.dimcard@gmail.com>
2024-04-15 17:21:25 +0300

    design changes

--- client/src/pages/registrationPage/registrationPage.jsx
+++ client/src/pages/registrationPage/registrationPage.jsx
@@ -3,8 +3,10 @@ import styles from "./registrationPage.module.css";
 
 const RegistrationPage = () => {
   const [defaultInput, setDefaultInput] = useState("youremail@domain.com");
-  const onEmailClick = () => {
-    setDefaultInput("");
+  const onEmailClick = (e) => {
+    if (e.target.value === "youremail@domain.com") {
+      setDefaultInput("");
+    }
   };
   const onEmailType = (e) => {
     setDefaultInput(e.target.value);
@@ -16,7 +18,7 @@ const RegistrationPage = () => {
           type="text"
           value={defaultInput}
           onChange={(e) => onEmailType(e)}
-          onClick={onEmailClick}
+          onClick={(e) => onEmailClick(e)}
         />
         <input type="password" />
       </div>
