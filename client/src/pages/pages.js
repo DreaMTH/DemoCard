719964418356a19dbfdca5e282439643ad8fd9b5
dreamth <d.dimcard@gmail.com>
2024-04-16 12:05:34 +0300

    initial commit

--- client/src/pages/pages.js
+++
@@ -1,6 +0,0 @@
-import HomePage from "./homePage/homePage.jsx";
-import LoginPage from "./loginPage/loginPage.jsx";
-import RegistrationPage from "./registrationPage/registrationPage.jsx";
-import UsersPage from "./usersPage/usersPage.jsx";
-
-export { HomePage, LoginPage, RegistrationPage, UsersPage };
--- client/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { usersReducer } from "../pages/usersPage/usersSlice.js";
-export default configureStore({
-  reducer: {
-    users: usersReducer,
-  },
-});
